e8b7db6f226f82cfc64dbd52efb1b4a205192e15
Natalia Chies <nataliachies@gmail.com>
2023-08-27 00:39:15 -0300

    authors

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,14 @@
 
 export const SITE_TITLE = "BioBath";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+
+
+export const authors = {
+    james: {
+        name: "James",
+        bio: "I'm a software engineer and I like to write about code.",
+        avatar: "/images/james.jpg",
+        twitter: "james",
+        github: "james",
+    },
+}
