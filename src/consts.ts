4d3d1cf14a95b63b628e759387b1623267acbda2
Natalia Chies <nataliachies@gmail.com>
2023-08-27 00:51:51 -0300

    authors

--- src/consts.ts
+++ src/consts.ts
@@ -6,11 +6,32 @@ export const SITE_DESCRIPTION = "Welcome to my website!";
 
 
 export const authors = {
-    james: {
-        name: "James",
-        bio: "I'm a software engineer and I like to write about code.",
-        avatar: "/images/james.jpg",
+    ntlchs: {
+        name: "Natália Chies",
+        bio: "I'm a full stack dev and I like to write about code.",
+        avatar: "/images/nat.jpg",
         twitter: "james",
-        github: "james",
+        github: "https://github.com/ntlchs",
+    },
+    luism6n: {
+        name: "Luis M.",
+        bio: "I'm a full stack dev and I like to write about code.",
+        avatar: "/images/luis.jpg",
+        twitter: "james",
+        github: "https://github.com/luism6n",
+    },
+    marceloprates: {
+        name: "Marcelo Prates",
+        bio: "I'm a full stack dev and I like to write about code.",
+        avatar: "/images/marcelo.jpg",
+        twitter: "james",
+        github: "https://github.com/marceloprates",
+    },
+    yahgo: {
+        name: "Yahgo",
+        bio: "I'm a full stack dev and I like to write about code.",
+        avatar: "/images/yahgo.jpg",
+        twitter: "james",
+        github: "https://github.com/yahgo",
     },
 }
